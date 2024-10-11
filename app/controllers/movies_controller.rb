d48f44f78879226aa038cc406038918ac88c34a5
Thieu Luu <96815689+tluu5@users.noreply.github.com>
2024-10-11 21:20:16 +0000

    final commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -20,9 +20,8 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.new
-    @movie.title = params.fetch(:movie).fetch(:title)
-    @movie.description = params.fetch(:movie).fetch(:description)
+    movie_attributes = params.require(:movie).permit(:title, :description)
+    @movie = Movie.new(movie_attributes)
 
     if @movie.valid?
       @movie.save
@@ -37,10 +36,12 @@ class MoviesController < ApplicationController
   end
 
   def update
-    @movie = Movie.find(params.fetch(:id))
+    movie_attributes = params.require(:movie).permit(:title, :description)
+    @movie = Movie.new(movie_attributes)
+    #@movie = Movie.find(params.fetch(:id))
 
-    @movie.title = params.fetch(:title)
-    @movie.description = params.fetch(:description)
+    #@movie.title = params.fetch(:title)
+    #@movie.description = params.fetch(:description)
 
     if @movie.valid?
       @movie.save
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,23 @@
 Rails.application.routes.draw do
   root "movies#index"
+  resources :movies
 
   # Routes for the Movie resource:
 
   # CREATE
-  post "/movies" => "movies#create", as: :movies
-  get "/movies/new" => "movies#new", as: :new_movie
+  #post "/movies" => "movies#create", as: :movies
+  #get "/movies/new" => "movies#new", as: :new_movie
           
   # READ
-  get "/movies" => "movies#index" 
-  get "/movies/:id" => "movies#show", as: :movie
+  #get "/movies" => "movies#index" 
+  #get "/movies/:id" => "movies#show", as: :movie
   
   # UPDATE
-  patch "/movies/:id" => "movies#update" 
-  get "/movies/:id/edit" => "movies#edit", as: :edit_movie 
+  #patch "/movies/:id" => "movies#update" 
+  #get "/movies/:id/edit" => "movies#edit", as: :edit_movie 
   
   # DELETE
-  delete "/movies/:id" => "movies#destroy"
+  #delete "/movies/:id" => "movies#destroy"
 
   #------------------------------
 end
